e204797f08b592106d49102dca415c2f1c52146d
xiangsong <1638392300@qq.com>
2021-04-06 00:08:53 +0800

    fix

--- dive-in-spring-boot/spring-webmvc/src/main/java/com/xschen/springboot/diveinspringboot/spring/webmvc/config/WebMvcConfig.java
+++ dive-in-spring-boot/spring-webmvc/src/main/java/com/xschen/springboot/diveinspringboot/spring/webmvc/config/WebMvcConfig.java
@@ -1,9 +1,73 @@
 package com.xschen.springboot.diveinspringboot.spring.webmvc.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.*;
+import org.springframework.web.servlet.config.annotation.DelegatingWebMvcConfiguration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.Locale;
+import java.util.Map;
+
 /**
+ * Spring Mvc 配置类
  * @author xschen
+ * 
+ * DispatcherServlet#doDispatch(HttpServletRequest, HttpServletResponse)
+ *  DispatcherServlet#getHandler(HttpServletRequest) 1
+ *  DispatcherServlet#getHandlerAdapter(Object) 2
+ *  DispatcherServlet#processDispatchResult 3
+ *  DispatcherServlet#render(ModelAndView, HttpServletRequest, HttpServletResponse) 4
+ *  DispatcherServlet#resolveViewName(String, Map, Locale, HttpServletRequest) 5
+ *
+ *
+ * @see DelegatingWebMvcConfiguration
+ * @see WebMvcConfigurer
+ * @see WebMvcConfigurer#addInterceptors(InterceptorRegistry)
  */
 
-public class WebMvcConfig {
 
+@Configuration
+@EnableWebMvc
+public class WebMvcConfig implements WebMvcConfigurer {
+
+    /**
+     *  <bean class="org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping"/>
+     *
+     *     <bean class="org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerAdapter"/>
+     *
+     *     <bean id="viewResolver" class="org.springframework.web.servlet.view.InternalResourceViewResolver">
+     *         <property name="viewClass" value="org.springframework.web.servlet.view.JstlView"/>
+     *         <property name="prefix" value="/WEB-INF/jsp/"/>
+     *         <property name="suffix" value=".jsp"/>
+     * </bean>
+     */
+
+    @Bean
+    public ViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setViewClass(JstlView.class);
+        viewResolver.setPrefix("/WEB-INF/jsp/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new HandlerInterceptor() {
+            @Override
+            public boolean preHandle(HttpServletRequest request,
+                                     HttpServletResponse response,
+                                     Object handler) throws Exception {
+                System.out.println("preHandle拦截...");
+                return true;
+            }
+        });
+    }
 }
--- dive-in-spring-boot/spring-webmvc/src/main/java/com/xschen/springboot/diveinspringboot/spring/webmvc/controller/HelloWorldController.java
+++ dive-in-spring-boot/spring-webmvc/src/main/java/com/xschen/springboot/diveinspringboot/spring/webmvc/controller/HelloWorldController.java
@@ -1,6 +1,10 @@
 package com.xschen.springboot.diveinspringboot.spring.webmvc.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
@@ -10,8 +14,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloWorldController {
 
-    @RequestMapping("/")
-    public String index() {
+    @RequestMapping("")
+    public String index(Model model) {
+        //model.addAttribute("acceptLanguage",acceptLanguage);
+        //model.addAttribute("jsessionId",jsessionId);
+        model.addAttribute("message","Hello,World");
         return "index";
+    }
+
+    @ModelAttribute("acceptLanguage")
+    public String acceptLanguage(@RequestHeader("Accept-Language") String acceptLanguage) {
+        return acceptLanguage;
+    }
+
+    @ModelAttribute("jsessionId")
+    public String jsessionId(@CookieValue("JSESSIONID") String jsessionId) {
+        return jsessionId;
     }
 }
